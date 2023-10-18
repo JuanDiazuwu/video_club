3b903092292fb156c3bbb165abe5839b6948c5e5
JuanDiazuwu <124910151+JuanDiazuwu@users.noreply.github.com>
2023-10-17 21:05:52 -0600

    Update db.js

--- db.js
+++ db.js
@@ -16,8 +16,8 @@ const copyModel = require('./models/copy');
     3) Contraseña base de datos
     4) Objeto de configuracion ORM
 */
-const sequelize = new Sequelize('videoclub', 'root', 'uwu', {
-        host: 'db_mysql', // db_mysql, localhost
+const sequelize = new Sequelize('video_club', 'fuan', 'awuitadeuwu!1', {
+        host: 'videoclub.mysql.database.azure.com', // db_mysql, localhost
         dialect: 'mysql',
         retry: {
             max: 1000
